254712c4c9e3b8a374fb8d6481f3f6784765537d
mhaikalkal <isalhaikal12@gmail.com>
2022-05-08 16:44:17 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// component
+// from nya ganti buat liat yg lain
+import Answer from "./tutorial/1-useState/answer/5-useState-counter";
+
+function App() {
+  return (
+    <div className="container">
+      <Answer />
+    </div>
+  );
+}
+
+export default App;
