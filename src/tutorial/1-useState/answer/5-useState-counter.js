6b330420d68cc44f3003c57b4e7e2924bec71bcb
mhaikalkal <isalhaikal12@gmail.com>
2022-05-10 11:39:16 +0700

    using timeout/delay to set an useState with function in it [ setValue((param) => {return param}) ]

--- src/tutorial/1-useState/answer/5-useState-counter.js
+++ src/tutorial/1-useState/answer/5-useState-counter.js
@@ -7,12 +7,38 @@ const UseStateCounter = () => {
     setValue(0);
   };
 
+  const delayIncrease = () => {
+    setTimeout(() => {
+      // misal current value = 4, kita click 3x yg ini. 3 detik kemudian akan jadi 7,
+      // yang jadi masalah ketika kita udah click 3x, dan sebelum 3 detik ini.
+      // kita click yg incerease tanpa timeout. contoh :
+      // curr = 4 lalu kita eksekusi => delayIncrease, delayIncrease, increaseBiasa, delayIncrease.
+      // hasilnya 7, bukan 8.
+      // karena kita ambil value langsung, bukan value current
+      // setValue(value + 1);
+
+      // jadi caranya :
+      // jadi si prevValue / prevState (terserah namanya apa) ini otomatis mengambil value dari useState
+      setValue((prevValue) => {
+        return prevValue + 1;
+      });
+    }, 5000);
+  };
+
+  // contoh set value useState menggunakan function.
+  // setSomething(function(x) { return x }) / setSomething((x) => { return x })
+  const decreaseHandler = () => {
+    setValue((prevState) => {
+      return prevState - 1;
+    });
+  };
+
   return (
     <>
       <section style={{ margin: "4rem 0" }}>
         <h2>Regular Counter</h2>
         <h1>{value}</h1>
-        <button className="btn" onClick={() => setValue(value - 1)}>
+        <button className="btn" onClick={decreaseHandler}>
           Decrease
         </button>
         <button className="btn" onClick={resetHandler}>
@@ -20,6 +46,13 @@ const UseStateCounter = () => {
         </button>
         <button className="btn" onClick={() => setValue(value + 1)}>
           Increase
+        </button>
+      </section>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>Complex Counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={delayIncrease}>
+          Increase Later
         </button>
       </section>
     </>
